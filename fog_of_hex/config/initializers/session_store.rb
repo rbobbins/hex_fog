75e3cc8833467e363095cd59c3722bc64fbc6dfd
Rachel <rachelheidi@gmail.com>
2012-10-26 01:40:34 -0400

    renamed app and changed other stuff

---
+++ fog_of_hex/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fogofhex::Application.config.session_store :cookie_store, key: '_hex_displayer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fogofhex::Application.config.session_store :active_record_store
