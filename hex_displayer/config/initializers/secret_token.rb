2c767135b9991a2f2f33c5ed2a5c279b87181c94
Rachel <rachelheidi@gmail.com>
2012-10-26 02:25:16 -0400

    about to deploy, fo real

--- hex_displayer/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-HexDisplayer::Application.config.secret_token = '098ef16a65db9448d47ff8a030c737929c08d098037ae9b182abbdc78616457f8f29e8c9c1c79a15028d7e1e34bfa4b093e4d747f0560c2b7958e437213febfd'
